6af05b9985c61be361e7aacbd7c55354282b7553
Sergei Tachenov <sergei@tachenov.name>
2018-09-20 19:18:52 +0300

    II.16. Sequences

--- src/ii_collections/n16FlatMap.kt
+++ src/ii_collections/n16FlatMap.kt
@@ -1,18 +1,9 @@
 package ii_collections
 
-fun example() {
-
-    val result = listOf("abc", "12").flatMap { it.toList() }
-
-    result == listOf('a', 'b', 'c', '1', '2')
-}
-
 val Customer.orderedProducts: Set<Product> get() {
-    // Return all products this customer has ordered
-    todoCollectionTask()
+    return orders.asSequence().flatMap { it.products.asSequence() }.toSet()
 }
 
 val Shop.allOrderedProducts: Set<Product> get() {
-    // Return all products that were ordered by at least one customer
-    todoCollectionTask()
+    return customers.asSequence().flatMap { it.orderedProducts.asSequence() }.toSet()
 }
