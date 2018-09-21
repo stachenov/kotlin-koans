2cf2fda6f940219c740082499af834d0a3c82a45
Sergei Tachenov <sergei@tachenov.name>
2018-09-21 21:47:44 +0300

    V.37. String and map builders

--- src/v_builders/n37StringAndMapBuilders.kt
+++ src/v_builders/n37StringAndMapBuilders.kt
@@ -3,22 +3,6 @@ package v_builders
 import util.TODO
 import java.util.*
 
-fun buildStringExample(): String {
-    fun buildString(build: StringBuilder.() -> Unit): String {
-        val stringBuilder = StringBuilder()
-        stringBuilder.build()
-        return stringBuilder.toString()
-    }
-
-    return buildString {
-        this.append("Numbers: ")
-        for (i in 1..10) {
-            // 'this' can be omitted
-            append(i)
-        }
-    }
-}
-
 fun todoTask37(): Nothing = TODO(
     """
         Task 37.
@@ -29,11 +13,16 @@ fun todoTask37(): Nothing = TODO(
 )
 
 fun task37(): Map<Int, String> {
-    todoTask37()
-//    return buildMap {
-//        put(0, "0")
-//        for (i in 1..10) {
-//            put(i, "$i")
-//        }
-//    }
+    return buildMap {
+        put(0, "0")
+        for (i in 1..10) {
+            put(i, "$i")
+        }
+    }
+}
+
+fun buildMap(builder: MutableMap<Int, String>.() -> Unit): Map<Int, String> {
+    val result = HashMap<Int, String>()
+    result.builder()
+    return result
 }
