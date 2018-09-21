ecd3dde1a252d67e9bee01d1a0e3d20b748c8f77
Sergei Tachenov <sergei@tachenov.name>
2018-09-21 19:09:54 +0300

    III.31. Invoke

--- src/iii_conventions/n31Invoke.kt
+++ src/iii_conventions/n31Invoke.kt
@@ -3,7 +3,10 @@ package iii_conventions
 import util.TODO
 
 
-class Invokable
+class Invokable(private val numberOfInvocations: Int = 0) {
+    operator fun invoke() = Invokable(numberOfInvocations + 1)
+    fun getNumberOfInvocations() = numberOfInvocations
+}
 
 fun todoTask31(): Nothing = TODO(
     """
@@ -14,6 +17,5 @@ fun todoTask31(): Nothing = TODO(
     references = { invokable: Invokable -> })
 
 fun task31(invokable: Invokable): Int {
-    todoTask31()
-//    return invokable()()()().getNumberOfInvocations()
+    return invokable()()()().getNumberOfInvocations()
 }
